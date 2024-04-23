fc7fe944a39749e9978c7d73f20d7c5bf7e81b7b
kidalaz <buhryakov15@mail.ru>
2024-04-23 09:51:40 +0300

    :sparkles: added insert, update, delete subjects (admin privilege) and list subjects

---
+++ internal/routes/subject.go
@@ -0,0 +1,396 @@
+package routes
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"github.com/BukhryakovVladimir/learn_live/internal/model"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/lib/pq"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+func ListSubjects(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Invalid HTTP method. Only GET is allowed.", http.StatusMethodNotAllowed)
+		return
+	}
+
+	listSubjectsQuery := `SELECT id, subject_name FROM subject;`
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(queryTimeLimit)*time.Second)
+	defer cancel()
+
+	rows, err := db.QueryContext(ctx, listSubjectsQuery)
+	defer rows.Close()
+	if err != nil {
+		if errors.Is(ctx.Err(), context.DeadlineExceeded) {
+			log.Println("AddSubject QueryRowContext deadline exceeded: ", err)
+			http.Error(w, "Database query time limit exceeded", http.StatusGatewayTimeout)
+			return
+		}
+
+		var pgErr *pq.Error
+		if ok := errors.As(err, &pgErr); !ok {
+			log.Println("Internal server error: ", err)
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+		if pgErr.Code == "23505" {
+			log.Println("Unique key violation, subject already exists: ", err)
+			http.Error(w, "Subject already exists", http.StatusGatewayTimeout)
+			return
+		}
+
+		log.Println("Database error: ", pgErr)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	var subject model.Subject
+	var subjects []model.Subject
+
+	for rows.Next() {
+		if err := rows.Scan(&subject.ID, &subject.SubjectName); err != nil {
+			log.Println(err)
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
+		subjects = append(subjects, subject)
+	}
+
+	if err := rows.Err(); err != nil {
+		log.Println(err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	resp, err := json.Marshal(subjects)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_, err = w.Write(resp)
+	if err != nil {
+		log.Printf("List subject failed: %v\n", err)
+	}
+}
+
+func AddSubject(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid HTTP method. Only POST is allowed.", http.StatusMethodNotAllowed)
+		return
+	}
+
+	cookie, err := r.Cookie(jwtName)
+
+	if err != nil {
+		http.Error(w, "Unauthenticated", http.StatusUnauthorized)
+		return
+	}
+
+	token, err := jwtCheck(cookie)
+
+	if err != nil {
+		http.Error(w, "Unauthenticated", http.StatusUnauthorized)
+		return
+	}
+
+	claims := token.Claims.(*jwt.RegisteredClaims)
+
+	userExists, err := checkUserExists(claims.Issuer)
+	if err != nil {
+		http.Error(w, "Error while checking user authorization", http.StatusInternalServerError)
+		return
+	}
+
+	if !userExists {
+		log.Println("User with id ", claims.Issuer, "does not exist: ", err)
+		http.Error(w, "You are not logged in", http.StatusUnauthorized)
+		return
+	}
+
+	isAdmin, err := isAdmin(claims.Issuer)
+	if err != nil {
+		http.Error(w, "Error while checking administrator privileges", http.StatusInternalServerError)
+		return
+	}
+
+	if !isAdmin {
+		http.Error(w, "You do not have administrator privileges to add movies", http.StatusUnauthorized)
+		return
+	}
+
+	var subject model.Subject
+
+	err = json.NewDecoder(r.Body).Decode(&subject)
+	if err != nil {
+		http.Error(w, "Error reading request body", http.StatusInternalServerError)
+		return
+	}
+	defer r.Body.Close()
+
+	if len([]rune(subject.SubjectName)) > 255 {
+		http.Error(w, "Maximum subject name length is 255 characters", http.StatusBadRequest)
+		return
+	}
+
+	insertSubjectQuery := `INSERT INTO subject (subject_name) VALUES ($1::text);`
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(queryTimeLimit)*time.Second)
+	defer cancel()
+
+	_, err = db.ExecContext(ctx, insertSubjectQuery, subject.SubjectName)
+
+	if err != nil {
+		if errors.Is(ctx.Err(), context.DeadlineExceeded) {
+			log.Println("AddSubject QueryRowContext deadline exceeded: ", err)
+			http.Error(w, "Database query time limit exceeded", http.StatusGatewayTimeout)
+			return
+		}
+
+		var pgErr *pq.Error
+		if ok := errors.As(err, &pgErr); !ok {
+			log.Println("Internal server error: ", err)
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+		if pgErr.Code == "23505" {
+			log.Println("Unique key violation, subject already exists: ", err)
+			http.Error(w, "Subject already exists", http.StatusGatewayTimeout)
+			return
+		}
+
+		log.Println("Database error: ", pgErr)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	resp, err := json.Marshal("Inserting subject successful")
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	_, err = w.Write(resp)
+	if err != nil {
+		log.Printf("Write failed: %v\n", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+}
+
+func UpdateSubject(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPut {
+		http.Error(w, "Invalid HTTP method. Only PUT is allowed.", http.StatusMethodNotAllowed)
+		return
+	}
+
+	cookie, err := r.Cookie(jwtName)
+
+	if err != nil {
+		http.Error(w, "Unauthenticated", http.StatusUnauthorized)
+		return
+	}
+
+	token, err := jwtCheck(cookie)
+
+	if err != nil {
+		http.Error(w, "Unauthenticated", http.StatusUnauthorized)
+		return
+	}
+
+	claims := token.Claims.(*jwt.RegisteredClaims)
+
+	userExists, err := checkUserExists(claims.Issuer)
+	if err != nil {
+		http.Error(w, "Error while checking user authorization", http.StatusInternalServerError)
+		return
+	}
+
+	if !userExists {
+		log.Println("User with id ", claims.Issuer, "does not exist: ", err)
+		http.Error(w, "You are not logged in", http.StatusUnauthorized)
+		return
+	}
+
+	isAdmin, err := isAdmin(claims.Issuer)
+	if err != nil {
+		http.Error(w, "Error while checking administrator privileges", http.StatusInternalServerError)
+		return
+	}
+
+	if !isAdmin {
+		http.Error(w, "You do not have administrator privileges to add movies", http.StatusUnauthorized)
+		return
+	}
+
+	var subject model.Subject
+
+	err = json.NewDecoder(r.Body).Decode(&subject)
+	if err != nil {
+		http.Error(w, "Error reading request body", http.StatusInternalServerError)
+		return
+	}
+	defer r.Body.Close()
+
+	if len([]rune(subject.SubjectName)) > 255 {
+		http.Error(w, "Maximum subject name length is 255 characters", http.StatusBadRequest)
+		return
+	}
+
+	updateSubjectQuery := `UPDATE subject SET subject_name = $1::text WHERE id = $2;`
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(queryTimeLimit)*time.Second)
+	defer cancel()
+
+	_, err = db.ExecContext(ctx, updateSubjectQuery, subject.SubjectName, subject.ID)
+
+	if err != nil {
+		if errors.Is(ctx.Err(), context.DeadlineExceeded) {
+			log.Println("UpdateSubject QueryRowContext deadline exceeded: ", err)
+			http.Error(w, "Database query time limit exceeded", http.StatusGatewayTimeout)
+			return
+		}
+
+		var pgErr *pq.Error
+		if ok := errors.As(err, &pgErr); !ok {
+			log.Println("Internal server error: ", err)
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+		if pgErr.Code == "23505" {
+			log.Println("Unique key violation, subject already exists: ", err)
+			http.Error(w, "Subject already exists", http.StatusGatewayTimeout)
+			return
+		}
+
+		log.Println("Database error: ", pgErr)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	resp, err := json.Marshal("Update subject successful")
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	_, err = w.Write(resp)
+	if err != nil {
+		log.Printf("Update subject failed: %v\n", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+}
+
+func DeleteSubject(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodDelete {
+		http.Error(w, "Invalid HTTP method. Only DELETE is allowed.", http.StatusMethodNotAllowed)
+		return
+	}
+
+	cookie, err := r.Cookie(jwtName)
+
+	if err != nil {
+		http.Error(w, "Unauthenticated", http.StatusUnauthorized)
+		return
+	}
+
+	token, err := jwtCheck(cookie)
+
+	if err != nil {
+		http.Error(w, "Unauthenticated", http.StatusUnauthorized)
+		return
+	}
+
+	claims := token.Claims.(*jwt.RegisteredClaims)
+
+	userExists, err := checkUserExists(claims.Issuer)
+	if err != nil {
+		http.Error(w, "Error while checking user authorization", http.StatusInternalServerError)
+		return
+	}
+
+	if !userExists {
+		log.Println("User with id ", claims.Issuer, "does not exist: ", err)
+		http.Error(w, "You are not logged in", http.StatusUnauthorized)
+		return
+	}
+
+	isAdmin, err := isAdmin(claims.Issuer)
+	if err != nil {
+		http.Error(w, "Error while checking administrator privileges", http.StatusInternalServerError)
+		return
+	}
+
+	if !isAdmin {
+		http.Error(w, "You do not have administrator privileges to add movies", http.StatusUnauthorized)
+		return
+	}
+
+	idParam := r.URL.Query().Get("id")
+
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		http.Error(w, "id must be an integer", http.StatusBadRequest)
+		return
+	}
+
+	deleteSubjectQuery := `DELETE FROM subject WHERE id = $1;`
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(queryTimeLimit)*time.Second)
+	defer cancel()
+
+	_, err = db.ExecContext(ctx, deleteSubjectQuery, id)
+
+	if err != nil {
+		if errors.Is(ctx.Err(), context.DeadlineExceeded) {
+			log.Println("DeleteSubject QueryRowContext deadline exceeded: ", err)
+			http.Error(w, "Database query time limit exceeded", http.StatusGatewayTimeout)
+			return
+		}
+
+		var pgErr *pq.Error
+		if ok := errors.As(err, &pgErr); !ok {
+			log.Println("Internal server error: ", err)
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+		if pgErr.Code == "23505" {
+			log.Println("Unique key violation, subject already exists: ", err)
+			http.Error(w, "Subject already exists", http.StatusGatewayTimeout)
+			return
+		}
+
+		log.Println("Database error: ", pgErr)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	resp, err := json.Marshal("Delete subject successful")
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	_, err = w.Write(resp)
+	if err != nil {
+		log.Printf("Delete subject failed: %v\n", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+}
