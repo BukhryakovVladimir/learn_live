149ad6b1eb662db3ce0cf2289bc9c7a83e9190d1
kidalaz <buhryakov15@mail.ru>
2024-04-29 16:53:39 +0300

    Revert ":sparkles: added insert, update, delete group_subject relations (admin privilege). Added list group_subject relations, list groups that have a subject, subjects of a group"
    This reverts commit a0e0f11b3c56f2deabc9cbd48b69a43a986a6866.

--- internal/model/group_subject.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type GroupSubject struct {
-	GroupID     int    `json:"group_id"`
-	SubjectID   int    `json:"subject_id"`
-	GroupName   string `json:"group_name"`
-	SubjectName string `json:"subject_name"`
-}
--- internal/model/update_group_subject.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type UpdateGroupSubject struct {
-	OldGroupID   int `json:"old_group_id"`
-	OldSubjectID int `json:"old_subject_id"`
-	NewGroupID   int `json:"new_group_id"`
-	NewSubjectID int `json:"new_subject_id"`
-}
