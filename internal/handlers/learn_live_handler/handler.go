a0e0f11b3c56f2deabc9cbd48b69a43a986a6866
kidalaz <buhryakov15@mail.ru>
2024-04-29 16:48:03 +0300

    :sparkles: added insert, update, delete group_subject relations (admin privilege). Added list group_subject relations, list groups that have a subject, subjects of a group

--- internal/handlers/learn_live_handler/handler.go
+++ internal/handlers/learn_live_handler/handler.go
@@ -7,6 +7,9 @@ import (
 
 func SetupRoutes(mux *http.ServeMux) {
 	mux.HandleFunc("/api/signup", routes.SignupPerson)
+	//list students for professors and admins
+	//list professors for all users
+	//list admins is admin and professor privilege
 	//delete person
 	//update person
 	mux.HandleFunc("/api/login", routes.LoginPerson)
@@ -20,4 +23,11 @@ func SetupRoutes(mux *http.ServeMux) {
 	mux.HandleFunc("/api/add-group", routes.AddGroup)
 	mux.HandleFunc("/api/update-group", routes.UpdateGroup)
 	mux.HandleFunc("/api/delete-group", routes.DeleteGroup)
+
+	mux.HandleFunc("/api/list-groups-and-subjects-relations", routes.ListGroupsSubjects)      // list both ids and names
+	mux.HandleFunc("/api/list-subjects-of-a-group", routes.ListSubjectsOfAGroup)              // list both ids and names
+	mux.HandleFunc("/api/list-groups-that-have-a-subject", routes.ListGroupsThatHaveASubject) // list both ids and names
+	mux.HandleFunc("/api/add-groups-and-subjects-relation", routes.AddGroupSubject)           // just insert
+	mux.HandleFunc("/api/update-groups-and-subjects-relation", routes.UpdateGroupSubject)     // just update set names by id
+	mux.HandleFunc("/api/delete-groups-and-subjects-relation", routes.DeleteGroupSubject)     // just delete by ids (remember, no body)
 }
